8d7d25271255bae9fea537cd0a0d3582a1084385
AliAnderson <alexis.anderson@target.com>
2020-06-30 23:50:31 -0500

    add PyPoll code

---
+++ PyPoll/pypoll.py
@@ -0,0 +1,22 @@
+import pandas as pd 
+
+def main():
+
+    file = "election_data.csv"
+    small_file = "election_data_small.csv"
+    # Calculate the total number of votes
+
+    # List of candidates who received votes
+
+    # Percentage of votes each candidate won
+
+    # Total number of votes each candidate won
+
+    # The winner of the election based on popular vote
+
+    # Print analysis
+
+    # Create text file with results
+
+
+main()
